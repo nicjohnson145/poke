d6ee1da2187becc640d4abab862d592c8f84cc98
Nic Johnson <nicjohnson145@hotmail.com>
2023-02-24 13:54:16 -0600

    feat: empty GRPC scaffold

--- internal/grpc_executor.go
+++ internal/grpc_executor.go
@@ -1,1 +1,23 @@
 package internal
+
+import "github.com/rs/zerolog"
+
+type GRPCExecutorOpts struct {
+	Logger zerolog.Logger
+}
+
+func NewGRPCExecutor(opts GRPCExecutorOpts) *GRPCExecutor {
+	return &GRPCExecutor{
+		log: opts.Logger,
+	}
+}
+
+var _ Executor = (*GRPCExecutor)(nil)
+
+type GRPCExecutor struct {
+	log zerolog.Logger
+}
+
+func (g *GRPCExecutor) Execute(call Call) (*ExecuteResult, error) {
+	return nil, nil
+}
